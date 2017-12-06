5cd6d87f83e0e7e3f5ded6cb9e881245f0c2b220
Your Name <644187060@qq.com>
2017-12-06 19:25:20 +0800

    init toy code

--- app/src/main/java/com/slkk/okhttpsourceread/MainActivity.java
+++ app/src/main/java/com/slkk/okhttpsourceread/MainActivity.java
@@ -1,13 +1,56 @@
 package com.slkk.okhttpsourceread;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        NetAsyTask myTask = new NetAsyTask();
+        myTask.execute();
+
+    }
+    class NetAsyTask extends AsyncTask<String,String,String>{
+
+        @Override
+        protected String doInBackground(String... strings) {
+            Request request = new Request
+                    .Builder()
+                    .url("http://www.jianshu.com/p/0c383eaa5675")
+                    .build();
+
+            OkHttpClient okHttpClient = new OkHttpClient();
+            okHttpClient.newCall(request)
+                    .enqueue(new Callback() {
+                        @Override
+                        public void onFailure(Call call, IOException e) {
+                            Log.i(TAG, "onFailure: ");
+                        }
+
+                        @Override
+                        public void onResponse(Call call, Response response) throws IOException {
+                            Log.i(TAG, "onResponse: " + response.body().string());
+                        }
+                    });
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+        }
     }
 }
